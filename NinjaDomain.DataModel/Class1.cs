9537d4677a9770ff6105ec62db9c3d03f7fa24db
Joell <amamihechi.udu@decagon.dev>
2020-02-12 03:07:36 +0100

    Created dbsets of Ninja data

--- NinjaDomain.DataModel/Class1.cs
+++ NinjaDomain.DataModel/Class1.cs
@@ -1,7 +1,15 @@
-﻿namespace NinjaDomain.DataModel
+﻿using System.Data.Entity;
+
+using NinjaDomainClasses;
+
+namespace NinjaDomain.DataModel
 {
-    public class NinjaContext
+    public class NinjaContext : DbContext
     {
+        public DbSet<Ninja> Ninjas { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+        public DbSet<NinjaEquiptment> Equiptments { get; set; }
+
 
     }
 }
